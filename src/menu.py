0994ca73327e7e235bd88028174295cadd21be6a
Francielly Pazello <74922124+franpazello96@users.noreply.github.com>
2023-04-09 13:24:02 -0300

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -1,18 +1,15 @@
 import inicial
 import lru
 
-# Inicializa o cache vazio
-cache = []
-
 while True: 
     inicial.cabecalho("MENU PRINCIPAL")
     resposta = inicial.menu()
     if resposta == -1:
         print("modo de simulação")
-    if resposta == 0:
+    elif resposta == 0:
         print("Programa encerrado!")
         break
-    if resposta in range(1,100):
-        cache = lru.LRU(resposta, cache)
+    elif resposta in range(1,101):
+       lru.lista(resposta)
     else:
         print("Número inválido.")
